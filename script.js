d7183d74ba945ffec7fcaff044d1925136d08401
Usenmfon <usenmfonuko@gmail.com>
2021-09-25 14:07:40 +0100

    updated game ui and features

--- script.js
+++ script.js
@@ -2,34 +2,52 @@ let content = document.querySelector(".content");
 let tablet = document.querySelector(".numberTablet");
 let menu = document.querySelector(".menu");
 let number = document.getElementById("number");
-let container = document.querySelector('.container')
-let gameOver = document.createElement('div')
+let container = document.querySelector(".container");
+let gameOver = document.createElement("div");
+let replay = document.createElement("button");
+var input = document.getElementById("input_value");
+var buttons = document.querySelectorAll("button.number_button");
+var level_buttons = document.querySelectorAll(".level_button");
+var modal = document.getElementById("myModal")
+var btn = document.getElementById("myBtn")
+var span = document.getElementsByClassName("close")[0]
+var alert_message = document.getElementsByClassName("message")[0]
 let start = false;
 var rNumber;
 var randomize;
-var input = document.getElementById("input_value");
 var result;
-var buttons = document.querySelectorAll("button.number_button");
-var level_buttons = document.querySelectorAll(".level_button");
 var trials = 3;
+var speed;
+
 
 function startGame() {
-  if (!start) {
+  if (!start && (rNumber != null)) {
     content.style.display = "flex";
     menu.style.display = "none";
-   
+
     countDown();
     randNumber();
-    
   }
-  console.log("Its clicking");
+}
+
+// btn.onclick = function(){
+  // modal.style.display = "block";
+// }
+
+span.onclick = function() {
+  modal.style.display = "none"
+}
+
+window.onclick = function(event){
+  if(event.target == modal){
+    modal.style.display = "none"
+  }
 }
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function (event) {
     input.value = input.value + event.currentTarget.value;
     result = input.value;
-    console.log(result);
   });
 }
 
@@ -41,41 +59,45 @@ for (let i = 0; i < level_buttons.length; i++) {
 
 handleChange = (result) => {
   if (result == randomize) {
-    console.log("You win");
-    alert("You win!!!");
+    modal.style.display = "block";
+    alert_message.innerHTML = `Yea!!! You WIN. Congratulations`
   } else {
     --trials;
-    alert(`You have ${trials} trials remainingg`);
+    modal.style.display = "block";
+    alert_message.innerHTML = `You have ${trials} trials remaining`
+    // alert(`You have ${trials} trials remainingg`);
   }
 };
 
 const isGameOver = (val) => {
-    if(val < 1){
-        container.appendChild(gameOver)
-    gameOver.classList.add('gameOver')
-    gameOver.textContent = "Game Over"
+  if (val < 1) {
+    container.appendChild(gameOver);
+    container.appendChild(replay)
+    replay.classList.add("replay")
+    replay.textContent = "Replay?"
+    gameOver.classList.add("gameOver");
+    gameOver.textContent = "Game Over";
     content.style.display = "none";
-    }
+  }
+};
+
+replay.onclick = function(){
+  window.location.reload()
 }
 
 document.getElementById("submitForm").addEventListener("submit", function (e) {
   e.preventDefault();
   handleChange(result);
-  isGameOver(trials)
+  isGameOver(trials);
 });
 
 function randNumber() {
   randomize = Math.floor(Math.random() * rNumber);
+  console.log(randomize)
   tablet.innerHTML = `Choose a number between 1 to ${rNumber}`;
-  console.log(randomize);
 }
 
-// function handleChange(val){
-
-// }
-
 const countDown = function () {
-  var speed;
   if (rNumber <= 10) {
     speed = 300;
   } else if (rNumber <= 50) {
